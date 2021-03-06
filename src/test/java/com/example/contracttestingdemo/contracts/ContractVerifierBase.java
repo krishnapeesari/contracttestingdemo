75a065aa12412a6399aa7485739f6ccf658ef854
krishnapeesari <32824528+krishnapeesari@users.noreply.github.com>
2021-03-06 13:35:27 -0800

    Update ContractVerifierBase.java

--- src/test/java/com/example/contracttestingdemo/contracts/ContractVerifierBase.java
+++ src/test/java/com/example/contracttestingdemo/contracts/ContractVerifierBase.java
@@ -1,6 +1,7 @@
 package com.example.contracttestingdemo.contracts;
 
 import com.example.contracttestingdemo.ContractTestingDemoApplication;
+import com.github.tomakehurst.wiremock.client.WireMock;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.context.WebApplicationContext;
@@ -22,7 +25,14 @@ public abstract class ContractVerifierBase {
     private WebApplicationContext context;
     @BeforeEach
     public void setUp() {
+        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/api/users/2")).willReturn(
+                WireMock.aResponse()
+                        .withStatus(200)
+                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withBody("{\"data\":{\"id\":2,\"email\":\"testAccount@gmail.com\",\"first_name\":\"Krishna\",\"last_name\":\"Peesari\"}}")));
+
         RestAssuredMockMvc.webAppContextSetup(context);
+
     }
 
     @AfterEach
