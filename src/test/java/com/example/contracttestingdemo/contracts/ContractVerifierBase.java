c50cac5d5d734f928ab9300e0842d1f40df895ba
Krishna Reddy Peesari <kpeesari@expediagroup.com>
2021-03-08 22:29:49 -0800

    Bug fix

--- src/test/java/com/example/contracttestingdemo/contracts/ContractVerifierBase.java
+++ src/test/java/com/example/contracttestingdemo/contracts/ContractVerifierBase.java
@@ -1,7 +1,6 @@
 package com.example.contracttestingdemo.contracts;
 
 import com.example.contracttestingdemo.ContractTestingDemoApplication;
-import com.github.tomakehurst.wiremock.client.WireMock;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,8 +8,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.context.WebApplicationContext;
@@ -21,18 +18,13 @@ import org.springframework.web.context.WebApplicationContext;
 @ContextConfiguration(classes = ContractTestingDemoApplication.class,
         initializers = ContractTestApplicationContextInitializer.class)
 public abstract class ContractVerifierBase {
+
     @Autowired
     private WebApplicationContext context;
+
     @BeforeEach
     public void setUp() {
-        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/api/users/2")).willReturn(
-                WireMock.aResponse()
-                        .withStatus(200)
-                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .withBody("{\"data\":{\"id\":2,\"email\":\"testAccount@gmail.com\",\"first_name\":\"Krishna\",\"last_name\":\"Peesari\"}}")));
-
         RestAssuredMockMvc.webAppContextSetup(context);
-
     }
 
     @AfterEach
